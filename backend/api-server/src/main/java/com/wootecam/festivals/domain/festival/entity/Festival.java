d72c6dea9f077e34f589b87d550526fe220fe0d9
minnim1010 <pms1010123@daum.net>
2024-11-10 21:05:25 +0900

    Ci#3/github action 수정
    build: code review 스크립트 삭제
    
    build: PR 연동 소스를 슬랙에서 디스코드로 수정
    
    build: upload 아티팩트 버전 업 (v2 > v4)
    
    test: 축제 목록 조회 관련 테스트 올바르게 수정
    
    - 축제 목록 조회 시 시작 시간 동일한 경우 ID 역순으로 정렬 결과 올바르도록 수정
    
    feat: 페스티벌 시작, 종료 시각 초 절삭 로직 추가
    
    feat: 티켓 생성 시 시각 관련 필드 초 이하 절삭 로직 추가
    
    test: 티켓 생성 시 시각 관련 필드 초 이하 절삭 후 검증 로직 추가

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/festival/entity/Festival.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/festival/entity/Festival.java
@@ -3,6 +3,7 @@ package com.wootecam.festivals.domain.festival.entity;
 import com.wootecam.festivals.domain.festival.util.FestivalValidator;
 import com.wootecam.festivals.domain.member.entity.Member;
 import com.wootecam.festivals.global.audit.BaseEntity;
+import com.wootecam.festivals.global.utils.DateTimeUtils;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -73,8 +74,8 @@ public class Festival extends BaseEntity {
         this.title = title;
         this.description = description;
         this.festivalImg = festivalImg;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.startTime = DateTimeUtils.normalizeDateTime(startTime);
+        this.endTime = DateTimeUtils.normalizeDateTime(endTime);
         this.festivalPublicationStatus =
                 festivalPublicationStatus == null ? FestivalPublicationStatus.PUBLISHED : festivalPublicationStatus;
         this.festivalProgressStatus =
