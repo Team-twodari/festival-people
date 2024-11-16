7d8fc1841100b56a4ae9ff58e4d2b47c3580502c
minnim1010 <pms1010123@daum.net>
2024-11-16 16:34:43 +0900

    test: 대기열 통과 후 티켓 구매 테스트 케이스 시나리오 추가

--- backend/e2e/src/test/java/E2eTest.java
+++ backend/e2e/src/test/java/E2eTest.java
@@ -149,13 +149,13 @@ class E2eTest extends E2eTestEnvironment {
                 .baseUri(API_SERVER_URL)
                 .contentType(ContentType.JSON)
                 .body(createTicketCreateRequest(ticketQuantity, LocalDateTime.now()))
-                        .filter(adminCookieFilter)
-                        .when()
-                        .post("/api/v1/festivals/" + festivalId + "/tickets")
-                        .then()
-                        .statusCode(201)
-                        .body("data.ticketId", notNullValue())
-                        .extract().response();
+                .filter(adminCookieFilter)
+                .when()
+                .post("/api/v1/festivals/" + festivalId + "/tickets")
+                .then()
+                .statusCode(201)
+                .body("data.ticketId", notNullValue())
+                .extract().response();
 
         int ticketId = ticketResponse.path("data.ticketId");
 
@@ -238,13 +238,192 @@ class E2eTest extends E2eTestEnvironment {
                 .body("data.purchaseStatus", equalTo("PURCHASED"))
                 .body("data.festival.festivalId", equalTo(festivalId));
 
-         // 남은 재고 정합성 확인
+        // 남은 재고 정합성 확인
         given()
                 .baseUri(API_SERVER_URL)
                 .contentType(ContentType.JSON)
                 .filter(userCookieFilter)
                 .when()
-                .get("/api/v1/festivals/"+ festivalId +"/tickets")
+                .get("/api/v1/festivals/" + festivalId + "/tickets")
+                .then()
+                .statusCode(200)
+                .body("data.tickets.remainStock[0]", equalTo(ticketQuantity - 1));
+    }
+
+    @Test
+    @DisplayName("회원은 대기열을 통과하면 티켓을 구매할 수 있다")
+    void testFestivalTicketPurchaseThroughQueueScenario() throws InterruptedException, JsonProcessingException {
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createMemberCreateRequest("admin", "admin@email.com"))
+                .when()
+                .post("/api/v1/member/signup")
+                .then()
+                .statusCode(201);
+
+        // Step 2: Admin 로그인 후 세션 쿠키 얻기
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createMemberLoginRequest("admin@email.com"))
+                .filter(adminCookieFilter)
+                .when()
+                .post("/api/v1/auth/login")
+                .then()
+                .statusCode(200);
+
+        // Step 3: Admin 축제 생성
+        Response festivalResponse = given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createFestivalCreateRequest(LocalDateTime.now().plusDays(7)))
+                .filter(adminCookieFilter)
+                .when()
+                .post("/api/v1/festivals")
+                .then()
+                .statusCode(201)
+                .body("data.festivalId", notNullValue())
+                .extract().response();
+
+        int festivalId = festivalResponse.path("data.festivalId");
+
+        // Step 4: Admin 티켓 생성
+        int ticketQuantity = 100;
+        Response ticketResponse = given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createTicketCreateRequest(ticketQuantity, LocalDateTime.now()))
+                .filter(adminCookieFilter)
+                .when()
+                .post("/api/v1/festivals/" + festivalId + "/tickets")
+                .then()
+                .statusCode(201)
+                .body("data.ticketId", notNullValue())
+                .extract().response();
+
+        int ticketId = ticketResponse.path("data.ticketId");
+
+        // Step 5: User 회원가입
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createMemberCreateRequest("user", "user@email.com"))
+                .when()
+                .post("/api/v1/member/signup")
+                .then()
+                .statusCode(201);
+
+        // Step 6: User 로그인 후 세션 쿠키 얻기
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .body(createMemberLoginRequest("user@email.com"))
+                .filter(userCookieFilter)
+                .when()
+                .post("/api/v1/auth/login")
+                .then()
+                .statusCode(200);
+
+        // Step 7: User 축제 상세 조회
+        given()
+                .baseUri(API_SERVER_URL)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/festivals/" + festivalId)
+                .then()
+                .statusCode(200)
+                .body("data.festivalId", equalTo(festivalId));
+
+        // 대기열 참가
+        Response waitOrderResponse = given()
+                .baseUri(QUEUE_SERVER_URL)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/festivals/" + festivalId + "/tickets/" + ticketId + "/purchase/wait")
+                .then()
+                .statusCode(200)
+                .extract().response();
+        long absoluteWaitOrder = Long.parseLong(waitOrderResponse.path("data.absoluteWaitOrder").toString());
+
+        // 대기열 대기
+        int retry = 0;
+        for (; retry < 5; ++retry) {
+            waitOrderResponse = given()
+                    .baseUri(QUEUE_SERVER_URL)
+                    .filter(userCookieFilter)
+                    .when()
+                    .get("/api/v1/festivals/" + festivalId + "/tickets/" + ticketId + "/purchase/wait?waitOrder="
+                            + absoluteWaitOrder)
+                    .then()
+                    .statusCode(200)
+                    .extract().response();
+            String purchasable = waitOrderResponse.path("data.purchasable").toString();
+
+            if("true".equals(purchasable)) {
+                break;
+            }
+        }
+
+        if (retry == 5) {
+            throw new RuntimeException("대기열 통과 실패");
+        }
+
+        // Step 8: User 티켓 구매 가능 여부 확인
+        Response purchaseSessionResponse = given()
+                .baseUri(API_SERVER_URL)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/festivals/" + festivalId + "/tickets/" + ticketId + "/purchase/check")
+                .then()
+                .statusCode(200)
+                .body("data.purchasable", equalTo(true))
+                .extract().response();
+        String purchaseSession = purchaseSessionResponse.path("data.purchaseSession").toString();
+
+        // Step 9: User 티켓 구매 미리보기 조회
+        given()
+                .baseUri(API_SERVER_URL)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/festivals/" + festivalId + "/tickets/" + ticketId + "/purchase/" + purchaseSession)
+                .then()
+                .statusCode(200)
+                .body("data.ticketQuantity", equalTo(ticketQuantity));
+
+        // Step 10: User 티켓 구매
+        given()
+                .contentType(ContentType.JSON)
+                .baseUri(API_SERVER_URL)
+                .filter(userCookieFilter)
+                .when()
+                .post("/api/v1/festivals/" + festivalId + "/tickets/" + ticketId + "/purchase/" + purchaseSession)
+                .then()
+                .statusCode(200)
+                .body("data.paymentId", notNullValue());
+
+        Thread.sleep(5000); // 결제 완료까지 대기
+
+        // assert
+        // 구매한 티켓 내역에 존재하는지 확인
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/member/tickets/" + ticketId)
+                .then()
+                .statusCode(200)
+                .body("data.purchaseStatus", equalTo("PURCHASED"))
+                .body("data.festival.festivalId", equalTo(festivalId));
+
+        // 남은 재고 정합성 확인
+        given()
+                .baseUri(API_SERVER_URL)
+                .contentType(ContentType.JSON)
+                .filter(userCookieFilter)
+                .when()
+                .get("/api/v1/festivals/" + festivalId + "/tickets")
                 .then()
                 .statusCode(200)
                 .body("data.tickets.remainStock[0]", equalTo(ticketQuantity - 1));
