ed93dee39ff3f64838fcc0d0252fddc5ab0f7b0d
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2025-01-01 02:22:23 +0900

    feat: 티켓 스케줄링 발급 시 레디스 스트림을 통한 이벤트 발급 로직 추가
    - Pending 상태 처리 로직 추가
    - 티켓 생성에 따른 스케줄링을 위한 스트림 소비 로직 추가
    - 티켓 생성에 따른 스케줄링을 위한 스트림 생산 로직 추가

---
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketScheduleEventProducer.java
@@ -0,0 +1,49 @@
+package com.wootecam.festivals.domain.ticket.service;
+
+import static com.wootecam.festivals.domain.ticket.constant.TicketRedisStreamConstants.TICKET_STREAM_KEY;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wootecam.festivals.domain.ticket.entity.Ticket;
+import com.wootecam.festivals.global.exception.GlobalErrorCode;
+import com.wootecam.festivals.global.exception.type.ApiException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.RecordId;
+import org.springframework.data.redis.connection.stream.StreamRecords;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TicketScheduleEventProducer {
+
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void sendEvent(Ticket ticket) {
+        log.info("Send event to redis: {}", ticket);
+
+        try {
+            String ticketToJson = objectMapper.writeValueAsString(ticket);
+
+            ObjectRecord<String, String> message = StreamRecords.newRecord()
+                    .ofObject(ticketToJson)
+                    .withStreamKey(TICKET_STREAM_KEY);
+
+            RecordId recordId = redisTemplate.opsForStream().add(message);
+
+            if (recordId == null) {
+                log.error("Failed to send ticket event to stream");
+                throw new ApiException(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+            }
+
+            log.info("Event sent to redis ticketId: {}", ticket.getId());
+        } catch (JsonProcessingException e) {
+            log.error("Failed to send ticket event to stream", e);
+            throw new ApiException(GlobalErrorCode.INTERNAL_SERVER_ERROR, e);
+        }
+    }
+}
---
+++ backend/schedule-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketScheduleConsumer.java
@@ -0,0 +1,91 @@
+package com.wootecam.festivals.domain.ticket.service;
+
+import static com.wootecam.festivals.domain.ticket.constant.TicketRedisStreamConstants.TICKET_STREAM_GROUP;
+import static com.wootecam.festivals.domain.ticket.constant.TicketRedisStreamConstants.TICKET_STREAM_KEY;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wootecam.festivals.domain.ticket.entity.Ticket;
+import com.wootecam.festivals.global.exception.GlobalErrorCode;
+import com.wootecam.festivals.global.exception.type.ApiException;
+import com.wootecam.festivals.global.utils.RedisStreamOperator;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+import org.springframework.data.redis.stream.Subscription;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TicketScheduleConsumer implements StreamListener<String, ObjectRecord<String, String>>, InitializingBean,
+        DisposableBean {
+
+    private final TicketScheduleService ticketScheduleService;
+    private final StringRedisTemplate redisTemplate;
+    private final RedisStreamOperator redisOperator;
+    private final ObjectMapper objectMapper;
+    private Subscription subscription;
+    private StreamMessageListenerContainer<String, ObjectRecord<String, String>> container;
+
+    @Override
+    public void onMessage(ObjectRecord<String, String> message) {
+        log.info("Received message: {}", message);
+
+        try {
+            Ticket ticket = objectMapper.readValue(message.getValue(), Ticket.class);
+            ticketScheduleService.scheduleRedisTicketInfoUpdate(ticket);
+
+            redisTemplate.opsForStream().acknowledge(TICKET_STREAM_KEY, TICKET_STREAM_GROUP, message.getId());
+            log.info("성공적으로 메시지 처리 ACK : {}", message.getId());
+        } catch (JsonProcessingException e) {
+            throw new ApiException(GlobalErrorCode.INTERNAL_SERVER_ERROR, "티켓 스트림 메시지 처리 중 예외 발생", e);
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("Starting TicketScheduleConsumer...");
+        // Consumer Group 설정
+        this.redisOperator.createStreamConsumerGroup(TICKET_STREAM_KEY, TICKET_STREAM_GROUP);
+
+        // StreamMessageListenerContainer 설정
+        this.container = this.redisOperator.createStreamMessageListenerContainer();
+
+        //Subscription 설정
+        this.subscription = this.container.receive(
+                Consumer.from(TICKET_STREAM_GROUP, "consumer-" + System.currentTimeMillis()),
+                StreamOffset.create(TICKET_STREAM_KEY, ReadOffset.lastConsumed()),
+                this
+        );
+
+        this.subscription.await(Duration.ofMillis(20));
+
+        // redis listen 시작
+        this.container.start();
+
+        if (this.container.isRunning()) {
+            log.info("TicketScheduleConsumer is running...");
+        }
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        log.info("Closing TicketScheduleConsumer...");
+        if (this.subscription != null) {
+            this.subscription.cancel();
+        }
+        if (this.container != null) {
+            this.container.stop();
+        }
+    }
+}
