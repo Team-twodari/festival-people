32390900ff293fc692d4e43990d025208bb167e5
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2025-01-24 01:00:39 +0900

    fix: Quartz가 스프링 DataSource로 초기화 되던 문제 해결
    - QuartzDataSource를 빈으로 명시적으로 해결

--- backend/schedule-server/src/main/java/com/wootecam/festivals/global/config/SchedulerConfig.java
+++ backend/schedule-server/src/main/java/com/wootecam/festivals/global/config/SchedulerConfig.java
@@ -1,8 +1,12 @@
 package com.wootecam.festivals.global.config;
 
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.spi.JobFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +19,18 @@ public class SchedulerConfig {
 
     private final ApplicationContext applicationContext;
 
+    @Value("${spring.quartz.properties.org.quartz.dataSource.schedule.URL}")
+    private String url;
+
+    @Value("${spring.quartz.properties.org.quartz.dataSource.schedule.user}")
+    private String username;
+
+    @Value("${spring.quartz.properties.org.quartz.dataSource.schedule.password}")
+    private String password;
+
+    @Value("${spring.quartz.properties.org.quartz.dataSource.schedule.driver}")
+    private String driverClassName;
+
     /**
      * JobFactory 설정 Quartz에서 Job을 생성할 때 Spring의 ApplicationContext를 사용하기 위한 설정
      */
@@ -23,5 +39,16 @@ public class SchedulerConfig {
         SpringBeanJobFactory jobFactory = new SpringBeanJobFactory();
         jobFactory.setApplicationContext(applicationContext);
         return jobFactory;
+    }
+
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource() {
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
     }
 }
