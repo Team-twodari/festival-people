4dd73305780af9fd9091239df871fae875fdd936
minnim1010 <pms1010123@daum.net>
2025-03-02 21:29:20 +0100

    feat(TimeProvider): 밀리초 단위 현재 시각 조회 메서드 추가

--- backend/core/src/main/java/com/wootecam/festivals/global/utils/TimeProvider.java
+++ backend/core/src/main/java/com/wootecam/festivals/global/utils/TimeProvider.java
@@ -1,5 +1,6 @@
 package com.wootecam.festivals.global.utils;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +9,9 @@ public class TimeProvider {
 
     public LocalDateTime getCurrentTime() {
         return LocalDateTime.now();
+    }
+
+    public long getCurrentTimeInMilli() {
+        return Instant.now().toEpochMilli();
     }
 }
