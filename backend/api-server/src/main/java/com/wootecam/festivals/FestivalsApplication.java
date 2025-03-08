91987a75734376e2fd3ca0d5c52bd59b7dc6b022
minnim1010 <pms1010123@daum.net>
2025-03-08 11:39:28 +0100

    refactor(FestivalsApplication): 불필요한 import문 제거

--- backend/api-server/src/main/java/com/wootecam/festivals/FestivalsApplication.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/FestivalsApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CloudConfiguration.class)
 @EnableAsync
 public class FestivalsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FestivalsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalsApplication.class, args);
+    }
 }
